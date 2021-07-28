a3f1fe7bf3621c23aa98548883ff4a8deedfbd73
James Ekstrom <60790649+james-ekstrom-slalom@users.noreply.github.com>
2021-07-28 14:53:09 -0700

    Create main.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "test_sg" {
+  vpc_id      = "12345"
+  name        = "test_sg"
+
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = [0.0.0.0/0]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
